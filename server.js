92eebaf198e7cd3e35c9ccefd56b8d7a8298067c
PRoy <sam469roger@gmail.com>
2024-08-12 07:00:38 +0530

    added asset route into server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
+const assetRoutes = require('./routes/assets');
 
 dotenv.config();
 connectDB();
@@ -11,7 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1/auth', authRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/assets', assetRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
