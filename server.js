014c7cfb4bc4ef0d79fd00ab3ca08ec97bdce314
PRoy <sam469roger@gmail.com>
2024-08-12 06:49:43 +0530

    implemented authRoutes into server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
 
 dotenv.config();
 connectDB();
@@ -9,6 +10,8 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/v1/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
